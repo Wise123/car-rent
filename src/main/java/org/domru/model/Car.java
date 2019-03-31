a4404d4ea7085be513335e90178922e47b824245
Wise <roman_nagaev_1993@mail.ru>
2019-03-31 18:52:33 +0500

    добавил связь автомобиля с моделью

--- src/main/java/org/domru/model/Car.java
+++ src/main/java/org/domru/model/Car.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -22,5 +24,8 @@ public class Car {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String regNum;
-  private Long carModelId;
+
+  @ManyToOne
+  @JoinColumn(name = "carModelId")
+  CarModel carModel;
 }
