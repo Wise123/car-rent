1dd64a8f2fa209327b7f2840e61b840b41e98449
Wise <roman_nagaev_1993@mail.ru>
2019-03-31 17:28:37 +0500

    настроил гибернейт, подготовил круд для производителя автомобилей

--- src/main/java/org/domru/controller/CarManufactorerController.java
+++ src/main/java/org/domru/controller/CarManufactorerController.java
@@ -2,9 +2,19 @@ package org.domru.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.domru.model.CarManufactorer;
+import org.domru.repository.CarManufactorerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +23,63 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(value = "car-manufactorer", description = "Контроллер для работы с производителями")
 public class CarManufactorerController {
 
-  @GetMapping("/test")
-  @ApiOperation(value = "тестовый метод")
-  public String helloWorld() {
-    return "hello";
+  @Autowired
+  CarManufactorerRepository carManufactorerRepository;
+
+  @GetMapping("")
+  @ApiOperation(value = "получить всех производителей")
+  public List<CarManufactorer> findAll() {
+    return carManufactorerRepository.findAll();
+  }
+
+  @PostMapping("")
+  @ApiOperation(value = "создать производителя")
+  public CarManufactorer create(
+      @RequestBody
+      CarManufactorer carManufactorer
+  ) {
+    carManufactorer.setId(null);
+    return carManufactorerRepository.save(carManufactorer);
+  }
+
+  /**
+   * TODO.
+   * @param carManufactorer TODO
+   * @param response TODO
+   * @return TODO
+   */
+  @PutMapping("")
+  @ApiOperation(value = "обновить производителя")
+  public CarManufactorer update(
+      @RequestBody
+          CarManufactorer carManufactorer,
+      HttpServletResponse response
+  ) {
+    if (carManufactorerRepository.findById(carManufactorer.getId()).isPresent()) {
+      return carManufactorerRepository.save(carManufactorer);
+    }
+    response.setStatus(404);
+    return null;
+  }
+
+  /**
+   * TODO.
+   * @param carManufactorer TODO
+   * @param response TODO
+   * @return TODO
+   */
+  @DeleteMapping("")
+  @ApiOperation(value = "удалить производителя")
+  public CarManufactorer delete(
+      @RequestBody
+          CarManufactorer carManufactorer,
+      HttpServletResponse response
+  ) {
+    if (carManufactorerRepository.findById(carManufactorer.getId()).isPresent()) {
+      carManufactorerRepository.delete(carManufactorer);
+      return carManufactorer;
+    }
+    response.setStatus(404);
+    return null;
   }
 }
