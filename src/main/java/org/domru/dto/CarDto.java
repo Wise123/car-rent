ee83ada44e568119ea95d3f8911373af63732497
Wise <roman_nagaev_1993@mail.ru>
2019-04-07 17:14:19 +0500

    поправил шаблон для проверки регистрационных номеров на валидность

--- src/main/java/org/domru/dto/CarDto.java
+++ src/main/java/org/domru/dto/CarDto.java
@@ -21,7 +21,7 @@ public class CarDto {
   @NotNull(message = "Регистрационный номер не может быть пустым")
   @Pattern(
       message = "Регистрационный номер не соответствует шаблону",
-      regexp = "^[АВЕКМНОРСТУХ]\\d{3}(?<!000)[АВЕКМНОРСТУХ]{2}\\d{2,3}$")
+      regexp = "^[АВЕКМНОРСТУХ]\\d{3}(?<!000)[АВЕКМНОРСТУХ]{2}RUS\\d{2,3}$")
   private String regNum;
   @NotNull(message = "Модель автомобиля не может быть пустой")
   private CarModelDto carModel;
